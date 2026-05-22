00109c06b78dba548f0f1392c01355602c9bb452
Sanket Jadhav <jsanket819@gmail.com>
2026-05-22 23:15:45 +0530

    Initial commit of core java practice project

---
+++ src/main/java/com/jsank/corejavapractice/CorejavapracticeApplication.java
@@ -0,0 +1,13 @@
+package com.jsank.corejavapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorejavapracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorejavapracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsank/corejavapractice/CorejavapracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsank.corejavapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorejavapracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
